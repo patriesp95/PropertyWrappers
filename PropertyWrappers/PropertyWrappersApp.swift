0ad033ff61615729220bf6ef161f3d4f05cd2818
Patricia M Espert <pmespert@gmail.com>
2026-07-13 09:04:20 +0200

    Initial Commit

---
+++ PropertyWrappers/PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersApp.swift
+//  PropertyWrappers
+//
+//  Created by Patricia M Espert on 13/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
