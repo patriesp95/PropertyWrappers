c6dcbb4364ef3a99ba98c4bc4f7bf9e42ee0c75a
Patricia Martinez Espert <patrimespert@outlook.com>
2026-07-17 06:21:12 +0200

    add changes

--- PropertyWrappers/Ejercicio5View.swift
+++ PropertyWrappers/Ejercicio5View.swift
@@ -13,19 +13,44 @@ struct Ejercicio5View: View {
         MyTask3(name: "Go Shopping", priority: false),
         MyTask3(name: "Go to the grocery store", priority: true),
     ]
-    
-    @State private var tempTask: MyTask3? 
-    
+
+    @State private var tempTask: MyTask3?
+
     @State private var showingSheet: Bool = false
     @State private var showSuccessMessage: Bool = false
     @State private var isEditing: Bool = false
+    @State private var isDeleting: Bool = false
 
     private var tasksNotFound: Bool {
         tasks.isEmpty
     }
-    
+
     private var tempTaskNotFound: Bool {
         tempTask == nil
+    }
+    
+    private var userPerformance: String {
+        if isEditing {
+            if isDeleting {
+                "Task deleted successfully"
+            } else {
+               "Task updated successfully"
+            }
+        } else {
+            "Task added successfully"
+        }
+    }
+    
+    private var userPerformanceButtonColor: Color {
+        if isEditing {
+            if isDeleting {
+                .red.opacity(0.9)
+            } else {
+                .blue.opacity(0.9)
+            }
+        } else {
+            .green.opacity(0.9)
+        }
     }
 
     var body: some View {
@@ -38,9 +63,10 @@ struct Ejercicio5View: View {
                     .padding()
 
                 Spacer()
-                
+
                 Button {
                     isEditing = false
+                    isDeleting = false
                     tempTask = nil
                     showingSheet.toggle()
                 } label: {
@@ -79,14 +105,15 @@ struct Ejercicio5View: View {
             }
 
         }
-        .sheet(isPresented: $showingSheet) {            
+        .sheet(isPresented: $showingSheet) {
             Ejercicio5ViewSheet(
                 onDelete: { task in
                     tasks.removeAll { $0.id == task.id }
                     print(tasks)
                 },
                 onSave: { task in
-                    if let index = tasks.firstIndex(where: { $0.id == task.id }) {
+                    if let index = tasks.firstIndex(where: { $0.id == task.id })
+                    {
                         tasks[index] = task
                     } else {
                         tasks.append(task)
@@ -94,9 +121,10 @@ struct Ejercicio5View: View {
                 },
                 messageIsShown: $showSuccessMessage,
                 userIsEditing: $isEditing,
+                userIsDeleting: $isDeleting,
                 tempTask: $tempTask
             )
-            .onAppear() {
+            .onAppear {
                 if !tempTaskNotFound {
                     isEditing = true
                 }
@@ -104,23 +132,23 @@ struct Ejercicio5View: View {
         }
         .overlay(alignment: .top) {
             if showSuccessMessage {
-                Text(isEditing ? "Task edited successfully" : "Task added successfully" )
-                    .padding()
-                    .background(isEditing ? .blue.opacity(0.9) : .green.opacity(0.9))
-                    .foregroundStyle(.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
-                    .padding(.top, 20)
-                    .transition(
-                        .asymmetric(
-                            insertion: .move(edge: .top).combined(
-                                with: .opacity
-                            ),
-                            removal: .move(edge: .leading).combined(
-                                with: .opacity
-                            )
+                Text(userPerformance)
+                .padding()
+                .background(userPerformanceButtonColor)
+                .foregroundStyle(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .padding(.top, 20)
+                .transition(
+                    .asymmetric(
+                        insertion: .move(edge: .top).combined(
+                            with: .opacity
+                        ),
+                        removal: .move(edge: .leading).combined(
+                            with: .opacity
                         )
                     )
+                )
             }
 
         }
@@ -132,7 +160,7 @@ struct Ejercicio5View: View {
                     showSuccessMessage = false
 
                 }
-               
+
             }
         }
 
@@ -143,81 +171,74 @@ struct Ejercicio5ViewSheet: View {
     @Environment(\.dismiss) private var dismiss
     let onDelete: (MyTask3) -> Void
     let onSave: (MyTask3) -> Void
-    
+
     @Binding var messageIsShown: Bool
     @Binding var userIsEditing: Bool
+    @Binding var userIsDeleting: Bool
     @Binding var tempTask: MyTask3?
 
     @State var title: String = ""
     @State var priority: Bool = false
-    
+
     var isTempTaskNameValid: Bool {
-        if let _tempTask = Binding($tempTask) {
-            return !_tempTask.name.wrappedValue.isEmpty
-        }
-        return false
+        !title.isEmpty
     }
-    
+
     var isNewTaskTitleValid: Bool {
         let newTitle = title.trimmingCharacters(in: .whitespacesAndNewlines)
         return !newTitle.isEmpty
 
     }
-    
+
     var isUserEditingATask: Bool {
         tempTask != nil
     }
-    
+
     var buttonOpacity: Double {
-        isUserEditingATask  && !isTempTaskNameValid || !isUserEditingATask && !isNewTaskTitleValid ? 0.5 : 1
+        isUserEditingATask && !isTempTaskNameValid
+            || !isUserEditingATask && !isNewTaskTitleValid ? 0.5 : 1
     }
-    
-    
+
     var body: some View {
         VStack {
             VStack(spacing: 20) {
-                Text(isUserEditingATask ? "Edit your task" : "Add a task" )
+                Text(isUserEditingATask ? "Edit your task" : "Add a task")
                     .font(.title)
                     .fontWeight(.bold)
                     .padding()
-                
+
                 TextField("Name of the task", text: $title)
                     .textFieldStyle(.roundedBorder)
                     .keyboardType(.default)
                     .textContentType(.name)
                     .textInputAutocapitalization(.sentences)
                     .autocorrectionDisabled()
-                    .onAppear {
-                        guard let _tempTask = Binding($tempTask) else { return }
-                        title = _tempTask.wrappedValue.name
-                    }
-                
+
                 Toggle(isOn: $priority) {
                     Text("Is prioritary")
                 }
                 .toggleStyle(.switch)
-                .onAppear {
-                    guard let _tempTask = Binding($tempTask) else { return }
-                    priority = _tempTask.wrappedValue.priority
-                }
-            
+
                 Button {
+                    let cleanTitle = title.trimmingCharacters(in: .whitespacesAndNewlines)
                     if !isUserEditingATask {
-                        onSave(MyTask3(name: title, priority: priority))
+                        onSave(MyTask3(name: cleanTitle, priority: priority))
                     } else {
                         if isTempTaskNameValid {
-                            guard let _tempTask = Binding($tempTask) else { return }
-                            _tempTask.wrappedValue.name = title
+                            guard let _tempTask = Binding($tempTask) else {
+                                return
+                            }
+                            _tempTask.wrappedValue.name = cleanTitle
                             _tempTask.wrappedValue.priority = priority
                             onSave(_tempTask.wrappedValue)
                         }
                     }
-                            
+
                     messageIsShown = true
                     dismiss()
 
                 } label: {
-                    Text(isUserEditingATask ? "Save Changes":"Save")
+                    Text(isUserEditingATask ? "Save Changes" : "Save")
                         .frame(maxWidth: .infinity)
                         .padding()
                         .background(.green.opacity(0.9))
@@ -226,7 +247,10 @@ struct Ejercicio5ViewSheet: View {
                         .shadow(radius: 5)
                         .padding(.top, 20)
                 }
-                .disabled(isUserEditingATask ? !isTempTaskNameValid : !isNewTaskTitleValid)
+                .disabled(
+                    isUserEditingATask
+                        ? !isTempTaskNameValid : !isNewTaskTitleValid
+                )
                 .opacity(buttonOpacity)
 
                 Button {
@@ -242,13 +266,14 @@ struct Ejercicio5ViewSheet: View {
                         .shadow(radius: 5)
                         .padding(.top, 20)
                 }
-                
+
                 if isUserEditingATask {
                     Button {
-                        if let _tempTask = Binding($tempTask) {
-                            onDelete(_tempTask.wrappedValue)
-                            dismiss()
-                        }
+                        userIsDeleting = true
+                        messageIsShown = true
+                        guard let _tempTask = Binding($tempTask) else { return }
+                        onDelete(_tempTask.wrappedValue)
+                        dismiss()
                     } label: {
                         Text("Delete")
                             .frame(maxWidth: .infinity)
@@ -260,19 +285,22 @@ struct Ejercicio5ViewSheet: View {
                             .padding(.top, 20)
                     }
                 }
-                
+
                 Spacer()
 
             }
             .padding([.horizontal, .vertical])
-            
+
             Spacer()
-            
+
         }
-    
+        .onAppear {
+            title = tempTask?.name ?? ""
+            priority = tempTask?.priority ?? false
+        }
+
     }
-                
-        
+
 }
 
 struct MyTask3: Identifiable, Hashable {
@@ -284,7 +312,4 @@ struct MyTask3: Identifiable, Hashable {
 #Preview {
     Ejercicio5View()
 }
-   
-    
-   
 
