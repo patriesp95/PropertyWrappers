9918069e1fafbc1b2d252391631d828c4b091e69
Patricia Martinez Espert <patrimespert@outlook.com>
2026-07-21 10:12:13 +0200

    add changes

---
+++ PropertyWrappers/Ejercicio7/Ejercicio7CreationView.swift
@@ -0,0 +1,18 @@
+//
+//  Ejercicio7CreationView.swift
+//  PropertyWrappers
+//
+//  Created by Patricia M Espert on 21/07/2026.
+//
+
+import SwiftUI
+
+struct Ejercicio7CreationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Ejercicio7CreationView()
+}
